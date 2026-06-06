188a2fcbe4a41ca65fec1b019d534ffb317664f3
Brian Haas <bhaas@broadinstitute.org>
2026-06-06 15:59:53 -0400

    removing igrov-1

--- QUANT_ONLY/IGROV-1_sim/run_quant_bmarking.sh
+++
@@ -1,2 +0,0 @@
-#$(git rev-parse --show-toplevel)/benchmarking/bmark_nb_runner.py --analysisType quant_only --truth_gtf reference_data/benchmark_full_annotations.wGene.gtf --truth_quant reference_data/benchmark_transcript_expression.tab
-$(git rev-parse --show-toplevel)/benchmarking/bmark_nb_runner.py --analysisType quant_only --truth_gtf reference_data/benchmark_full_annotations.wGene.gtf --truth_quant reference_data/benchmark_transcript_expression.tab
