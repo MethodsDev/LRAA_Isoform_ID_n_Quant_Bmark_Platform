d7d86eacbbe591fa78d9f49054fd88b1ac8d9efc
Brian Haas <bhaas@broadinstitute.org>
2026-06-06 17:45:35 -0400

    removing IGROV-1

--- DENOVO_ID/IGROV-1_sim/run_ref_annot_free_ID_bmarking.sh
+++
@@ -1,2 +0,0 @@
-$(git rev-parse --show-toplevel)/benchmarking/bmark_nb_runner.py --analysisType ref_free --truth_gtf reference_data/benchmark_full_annotations.wGene.gtf --truth_quant reference_data/benchmark_transcript_expression.tab
-
